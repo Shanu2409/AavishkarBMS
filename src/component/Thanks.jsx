2c0da86b99baf10757c76e84b386f298eb1d4aee
shanu2409 <shanusingh2409@gmail>
2023-09-14 00:41:44 +0530

    done

--- src/component/Thanks.jsx
+++ src/component/Thanks.jsx
@@ -5,7 +5,7 @@ const Thanks = () => {
     <div className="centered-image-container">
       <img
         src="https://th.bing.com/th/id/R.41d23e9b321a39530461716b511d2149?rik=bKm4ayyacNiRYg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fgreen-tick-png-green-tick-icon-image-14141-1000.png&ehk=3U0tpIObVeYbY2T%2b%2bzARLcBcgQNB1ZRTyxFNRackJhg%3d&risl=&pid=ImgRaw&r=0"
-        alt="Centered Image"
+        alt="Centered"
         className="centered-image"
         height={100}
       />
